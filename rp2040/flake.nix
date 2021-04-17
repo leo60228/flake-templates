9edcd885887f7713c7036ea4ba2a600a6d45a771
leo60228 <leo@60228.dev>
2021-04-17 18:43:16 -0400

    add rp2040 template

---
+++ rp2040/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nix-rp2040.url = "github:leo60228/nix-rp2040";
+  inputs.nix-jlink.url = "github:leo60228/nix-jlink";
+
+  outputs = { nixpkgs, flake-utils, nix-rp2040, nix-jlink, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (nix-rp2040.packages.${system}) pico-sdk;
+        inherit (nix-jlink.packages.${system}) jlink;
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ gcc-arm-embedded cmake jlink ];
+          buildInputs = [ pico-sdk ];
+        };
+      }
+    );
+}
